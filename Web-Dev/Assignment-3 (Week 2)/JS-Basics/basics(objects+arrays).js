338243a4e9917663f828aaa704b5c13ddcf52a20
Ahmad <ahmadbazaib7@gmail.com>
2026-02-13 17:57:34 +0530

    first 10 problems done they were tough, I need to work on more questions and get good at js Now the first thing would be to complete the week 2 assignment of js by monday as i need to work on week 3, 4 assignments also

--- Web-Dev/Assignment-3 (Week 2)/JS-Basics/basics(objects+arrays).js
+++ Web-Dev/Assignment-3 (Week 2)/JS-Basics/basics(objects+arrays).js
@@ -1,15 +1,163 @@
 "use strict";
 
+////////////////////////////////////////////
 // 1. Sum values in object arrays
 
-// Input
-const expenses = { food: [10, 20, 30], travel: [5, 15], bills: [40, 60] };
+// const expenses = { food: [10, 20, 30], travel: [5, 15], bills: [40, 60] };
 
-// const cost = [];
-const values = Object.values(expenses);
+// const result = {};
 
-console.log([...values]);
+// for (const [key, value] of Object.entries(expenses)) {
+//   result[key] = value.reduce((acc, n) => acc + n, 0);
+// }
 
-for (let i = 0; i < values.length; i++) {
-  const finalValues = [[...values[i]]];
+// console.log(result);
+
+////////////////////////////////////////////
+// 2. Count word occurrences in array
+
+// const data = ["apple", "banana", "apple", "orange", "banana", "apple"];
+
+// const counts = {};
+
+// for (const word of data) {
+//   if (counts[word]) {
+//     counts[word]++;
+//   } else {
+//     counts[word] = 1;
+//   }
+// }
+
+// console.log(counts);
+
+////////////////////////////////////////////
+// 3. Swap keys and values of object
+
+// const data = { a: "x", b: "y", c: "z" };
+
+// const result = {};
+
+// for (const [key, value] of Object.entries(data)) {
+//   // console.log(key, value);
+//   result[value] = key;
+// }
+
+// console.log(result);
+
+////////////////////////////////////////////
+// 4. Find the largest value key
+
+// const data = { a: 10, b: 50, c: 20 };
+
+// let maxValue = 0;
+// let maxKey = null;
+
+// for (const [key, value] of Object.entries(data)) {
+//   if (value >= maxValue) {
+//     maxValue = value;
+//     maxKey = key;
+//   }
+// }
+
+// console.log(maxKey);
+
+////////////////////////////////////////////
+// 5. Flatten object of arrays into one array
+
+// const data = { fruits: ["apple", "banana"], veggies: ["carrot", "pea"] };
+
+// const [valueOne, valueTwo] = Object.values(data);
+
+// const finalArr = [...valueOne, ...valueTwo];
+
+// //Modern Way
+// // const finalArr = Object.values(data).flat();
+
+// console.log(finalArr);
+
+////////////////////////////////////////////
+// 6. Group people by city
+
+// const data = [
+//   { name: "A", city: "Delhi" },
+//   { name: "B", city: "Mumbai" },
+//   { name: "C", city: "Delhi" },
+// ];
+
+// const result = {};
+
+// for (const value of Object.values(data)) {
+//   // actual storage location
+//   if (result[value.city]) {
+//     result[value.city].push(value.name);
+//   } else {
+//     result[value.city] = [value.name];
+//   }
+// }
+
+// console.log(result);
+
+////////////////////////////////////////////
+// 7. Filter object by values > 50
+
+// const data = { a: 20, b: 60, c: 40, d: 90 };
+
+// const result = {};
+
+// for (const [key, value] of Object.entries(data)) {
+//   if (value > 50) {
+//     result[key] = value;
+//   }
+// }
+
+// console.log(result);
+
+////////////////////////////////////////////
+// 8. Find student with highest average mark
+
+// const data = { A: [80, 90], B: [70, 75, 85] };
+
+// const result = {};
+
+// for (const [key, value] of Object.entries(data)) {
+//   result[key] = value.reduce((acc, cur) => acc + cur, 0) / value.length;
+// }
+
+// let maxValue = 0;
+// let maxKey = null;
+
+// for (const [key, value] of Object.entries(result)) {
+//   if (value > maxValue) {
+//     maxValue = value;
+//     maxKey = key;
+//   }
+// }
+
+// console.log(maxKey);
+
+////////////////////////////////////////////
+// 9. Unique values across all object arrays
+
+// const data = { x: [1, 2, 3], y: [2, 3, 4], z: [4, 5] };
+
+// const result = Object.values(data).flat();
+
+// const uniqueResult = [...new Set(result)];
+
+// console.log(uniqueResult);
+
+////////////////////////////////////////////
+// 10. Pick only given keys from object
+
+const obj = { name: "Rahul", age: 23, city: "Noida" };
+const keys = ["name", "city"];
+
+const result = {};
+
+for (const key of keys) {
+  if (key in obj) {
+    result[key] = obj[key];
+  }
 }
+
+console.log(result);
