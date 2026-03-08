a269bc9176c3153986fd27224d4c8774ce7cc939
Ahmad <ahmadbazaib7@gmail.com>
2026-03-08 08:03:42 +0530

    DSA Week 4 - Arrays started again after long time

---
+++ DSA/Assignment-5 (Week 4)/I_reverse.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int N;
+  cin >> N;  
+
+  long long A[N];
+  for(int i = 0; i < N; i++)
+    cin >> A[i];
+    
+  int i = 0, j = N-1;
+  while(i<=j){
+    swap(A[i], A[j]);
+    i++;
+    j--;
+  }
+
+  for(int i = 0; i < N; i++)
+    cout << A[i] << " ";
+  
+  return 0;
+}
