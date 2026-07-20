bfeac9675773514197d4c33b91428ec2e1dfaf14
Ahmad <ahmadbazaib7@gmail.com>
2026-07-20 21:12:17 +0530

    Added My solution for it and the code is almost the same but the for loop second condition is different but works the same

--- DSA/Assignment-3 (Week 2)/Q_crown.cpp
+++ DSA/Assignment-3 (Week 2)/Q_crown.cpp
@@ -25,5 +25,22 @@ int main()
     cout << endl;
   }
 
+  // My Solution
+  // int n;
+  // cin >> n;
+    
+  // for(int i = 1; i <= n; i++){
+  //   for(int j = 1; j <= i; j++){
+  //     cout << "*";
+  //   }
+  //   for(int j = 1; j <= ((n * 2) - (i * 2)); j++){
+  //     cout << " ";
+  //   }
+  //   for(int j = 1; j <= i; j++){
+  //     cout << "*";
+  //   }
+  //   cout << endl;
+  // }
+
   return 0;
 }
