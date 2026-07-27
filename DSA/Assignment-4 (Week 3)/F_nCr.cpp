6aeb5df9abb2d07b3b765d5748debb2008306c90
Ahmad <ahmadbazaib7@gmail.com>
2026-07-27 21:13:50 +0530

    Wrote my own solution without any help, but could have been better as we could have made the code not repeatable which is bad

--- DSA/Assignment-4 (Week 3)/F_nCr.cpp
+++ DSA/Assignment-4 (Week 3)/F_nCr.cpp
@@ -30,3 +30,40 @@ int main()
 
   return 0;
 }
+
+// My Solution
+// using ll = long long;
+
+// ll nCr(int n, int r){
+//     ll ans1 = 1, ans2 = 1, ans3 = 1, fAns = 1;
+//     ll nr = n - r;
+    
+//     for(int i = 1; i <= n; i++){
+//         ans1 *= i;
+//     }
+    
+//     for(int i = 1; i <= r; i++){
+//         ans2 *= i;
+//     }
+    
+//     for(int i = 1; i <= nr; i++){
+//         ans3 *= i;
+//     }
+    
+//     fAns = (ans1/(ans2 * ans3));
+    
+//     return fAns;
+    
+// }
+
+// int main()
+// {
+//     int n, r;
+//     cin >> n >> r;
+    
+//     ll fAns = nCr(n, r);
+    
+//     cout << fAns;
+    
+//     return 0;
+// }
