f7d68cbc0e77e433b4187b124d4100b72983a672
Ahmad <ahmadbazaib7@gmail.com>
2026-07-29 15:49:01 +0530

    uploaded

--- Web-Dev/Assignment-3 (Week 2)/01-js/easy/anagram.js
+++ Web-Dev/Assignment-3 (Week 2)/01-js/easy/anagram.js
@@ -7,18 +7,17 @@
   - `npm run test-anagram`
 */
 function isAnagram(str1, str2) {
-
   if (str1.length !== str2.length) {
-      return false;
+    return false;
   }
-
 
   function sortString(str) {
-      return str.toLowerCase().split('').sort().join('');
+    return str.toLowerCase().split("").sort().join("");
   }
-
 
   return sortString(str1) === sortString(str2);
 }
 
 module.exports = isAnagram;
+
+console.log(isAnagram("rasp", "spar"));
