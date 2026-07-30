4aa06735d85958c2247dd02f5c3ccd1ccf9b422d
Ahmad <ahmadbazaib7@gmail.com>
2026-07-30 08:42:08 +0530

    01-js easy problems done

--- Web-Dev/Assignment-3 (Week 2)/01-js/medium/times.js
+++ Web-Dev/Assignment-3 (Week 2)/01-js/medium/times.js
@@ -8,5 +8,18 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+  const start = new Date();
+
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+
+  const end = new Date();
+
+  const finalTime = (end - start) / 1000;
+
+  return finalTime;
 }
+
+console.log(calculateTime(100000000000));
