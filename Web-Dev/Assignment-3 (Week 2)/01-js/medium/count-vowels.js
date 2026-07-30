1b1450a0734041d791d1db717744316c3c5e4b0e
Ahmad <ahmadbazaib7@gmail.com>
2026-07-30 08:04:45 +0530

    medium problems started -> was able to solve first problem easily

--- Web-Dev/Assignment-3 (Week 2)/01-js/medium/count-vowels.js
+++ Web-Dev/Assignment-3 (Week 2)/01-js/medium/count-vowels.js
@@ -2,11 +2,28 @@
   Implement a function `countVowels` that takes a string as an argument and returns the number of vowels in the string.
   Note: Consider both uppercase and lowercase vowels ('a', 'e', 'i', 'o', 'u').
 
-  Once you've implemented the logic, test your code by running
+  Once you've implemented the logic, test your code by running 'npm run test-count-vowels'
 */
 
 function countVowels(str) {
-    // Your code here
+  // Your code here
+  let count = 0;
+  const finalStr = str.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+    if (
+      finalStr[i] === "a" ||
+      finalStr[i] === "e" ||
+      finalStr[i] === "i" ||
+      finalStr[i] === "o" ||
+      finalStr[i] === "u"
+    ) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 module.exports = countVowels;
+
+console.log(countVowels("hello"));
