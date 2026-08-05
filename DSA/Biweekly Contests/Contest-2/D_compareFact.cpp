4c778876a44db08bb882f14e383fd877a7c604bf
Ahmad <ahmadbazaib7@gmail.com>
2026-08-05 23:56:14 +0530

    Solved the problem because the solution was simpler than what i wrote before

--- DSA/Biweekly Contests/Contest-2/D_compareFact.cpp
+++ DSA/Biweekly Contests/Contest-2/D_compareFact.cpp
@@ -1,32 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int fact(int n){
-  int ans = 1;
-  if(n == 0){
-    return 1;
-  }else{
-    for(int i = 1; i <= n; i++){
-      ans *= i;
+// int fact(int n){
+//   int ans = 1;
+//   if(n == 0){
+//     return 1;
+//   }else{
+//     for(int i = 1; i <= n; i++){
+//       ans *= i;
+//     }
+
+//     return ans;
+//   }
+// }
+
+
+// int main(){
+//   int a, b;
+//   cin >> a >> b;
+
+//   int factA = fact(a);
+//   int factB = fact(b);
+
+//   if(factA == factB){
+//     cout << "Yes";
+//   }else{
+//     cout << "No";
+//   }
+
+//   return 0;
+// }
+
+int main() {
+    long long a, b;
+    cin >> a >> b;
+
+    if (a == b || (a == 0 && b == 1) || (a == 1 && b == 0)) {
+        cout << "Yes";
+    } else {
+        cout << "No";
     }
 
-    return ans;
-  }
-}
-
-
-int main(){
-  int a, b;
-  cin >> a >> b;
-
-  int factA = fact(a);
-  int factB = fact(b);
-
-  if(factA == factB){
-    cout << "Yes";
-  }else{
-    cout << "No";
-  }
-
-  return 0;
+    return 0;
 }
