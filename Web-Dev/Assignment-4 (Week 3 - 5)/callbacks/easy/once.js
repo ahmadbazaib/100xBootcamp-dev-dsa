dfd6643b078796213c0dbb9b8b6ed080538f9cb5
Ahmad <ahmadbazaib7@gmail.com>
2026-08-09 21:38:09 +0530

    feat(once): ensure fn executes only on first call, replay result on subsequent calls

--- Web-Dev/Assignment-4 (Week 3 - 5)/callbacks/easy/once.js
+++ Web-Dev/Assignment-4 (Week 3 - 5)/callbacks/easy/once.js
@@ -7,7 +7,27 @@
 // the callback with the same result (or error) from the first invocation.
 
 function once(fn) {
+  let flag = false;
+  let savedResult;
+  return function (...args) {
+    const callback = args.pop();
 
+    if (!flag) {
+      fn(...args, (err, data) => {
+        if (err) {
+          savedResult = err;
+          callback(err);
+        } else {
+          savedResult = data;
+          callback(null, data);
+        }
+      });
+
+      flag = true;
+    } else {
+      callback(null, savedResult);
+    }
+  };
 }
 
 module.exports = once;
